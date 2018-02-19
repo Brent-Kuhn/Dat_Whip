08a860ee52160e9cb8a7c4527586f55d9bd71b18
car <car@car.car>
2018-02-19 20:52:55 +0000

    Tested line following

--- test_scripts/Line_Follow.py
+++ test_scripts/Line_Follow.py
@@ -7,36 +7,36 @@ pub=None
 
 def callback(data):
     # the code for returning y and x should go here
-    x,y=data.split(",")
+    x,y=data.data.split(",")
+    x=float(x)
+    y=float(y)
     drive(x,y)
-	pass
 
 
 def drive(x,y):
     global pub
-    direction = (-.34) * math.atan2(y, x)/ 1.57 # Assuming pos is left and neg is right
-    speed = y / 188  #change this value to the max length of a line
+    direction = (.34) * math.atan2(y, x)/ 1.57 # Assuming pos is left and neg is right
+    speed = y / 100  #change this value to the max length of a line
 
-	drive_msg_stamped = AckermannDriveStamped()
-	drive_msg = AckermannDrive()
-   	drive_msg.speed = speed
+    drive_msg_stamped = AckermannDriveStamped()
+    drive_msg = AckermannDrive()
+    drive_msg.speed = speed 
     drive_msg.steering_angle = direction
     drive_msg.acceleration = 0
     drive_msg.jerk = 0
     drive_msg.steering_angle_velocity = 0
-	drive_msg_stamped.drive = drive_msg
-	pub.publish(drive_msg_stamped)
+    drive_msg_stamped.drive = drive_msg
+    pub.publish(drive_msg_stamped)
 
 def main():
     global pub
-    rospy.init_node("lineListen",anonymous=False)
-    rospy.Subscriber("lineCords",String,callback)
-    rp.init_node("plzWerk",anonymous = False)
-	pub=rp.Publisher("/vesc/ackermann_cmd_mux/input/navigation",AckermannDriveStamped,queue_size=10)
-    rospy.spin()
+    rp.init_node("lineSteer",anonymous=False)
+    rp.Subscriber("lineCoords",String,callback)
+    pub=rp.Publisher("/vesc/ackermann_cmd_mux/input/navigation",AckermannDriveStamped,queue_size=10)
+    rp.spin()
 
 if __name__ =="__main__":
-	try:
+    try:
     	main()
-	except rp.ROSInterruptException:
-		pass
+    except rp.ROSInterruptException:
+        pass
