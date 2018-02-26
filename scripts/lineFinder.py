c2855894a6ccacf1b21bf3b2330aeb330e944e41
car <car@car.car>
2018-02-26 19:53:49 +0000

    Both cameras

--- scripts/lineFinder.py
+++ scripts/lineFinder.py
@@ -1,54 +1,81 @@
 #!/usr/bin/python
 import rospy as rp
+from sensor_msgs.msg import Image
 from std_msgs.msg import String
-from sensor_msgs.msg import LaserScan
-from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
-from classes.lineDriveClass import LineDrive
-from classes.estopClass import estop
+from cv_bridge import CvBridge, CvBridgeError
+from classes.lineFinderClass import LineFinder
 
-class steeringControl:
+class LineFinderSubscriber:
     def __init__(self):
-        rp.init_node("lineSteer",anonymous=False)
-        self.lineDriver=LineDrive(30)
-        self.estop=estop(30,240,841)
-        self.subscribeToLine()
-        #self.subscribeToScan()
-        self.pub=rp.Publisher("/vesc/ackermann_cmd_mux/input/navigation",AckermannDriveStamped,queue_size=10)
+        rp.init_node('image_processor', anonymous=False)
+        self.pub = rp.Publisher('lineCoords', String, queue_size=10)
+        self.lastLeftGoal = (0, 0)
+        self.lastRightGoal = (0, 0)
+        self.lineFinder = LineFinder()
+        self.bridge = CvBridge()
+        self.subscribeToBoth()
+
+    def subscribeToBoth(self):
+        self.subscribeToLeft()
+        self.subscribeToRight()
         rp.spin()
 
-    def subscribeToLine(self):
-        rp.Subscriber("lineCoords",String,self.camCallback)
+    def subscribeToLeft(self):
+        self.subscribeTo('zedLeft', self.zedLeftCallback)
 
-    def subscribeToScan(self):
-        rp.Subscriber("scan",LaserScan,self.scanCallback)
+    def subscribeToRight(self):
+        self.subscribeTo('zedRight', self.zedRightCallback)
 
-    def camCallback(self,lineData):
+    def subscribeTo(self, channel, callback):
+        rp.Subscriber(channel, Image, callback)
+
+    def zedLeftCallback(self, data):
+        image = self.getCVImageFromData(data)
+        line = self.lineFinder.findIn(image)
+        height, width, _ = image.shape
+        goalX, goalY = self.getGoalXYFromLine(line, width, height)
+        # self.publishXY(goalX, goalY)
+        self.lastLeftGoal = (goalX, goalY)
+
+        # Actually publish stuff
+        goalX, goalY = self.averageGoal()
+        self.publishXY(goalX, goalY)
+
+    def zedRightCallback(self, data):
+        image = self.getCVImageFromData(data)
+        line = self.lineFinder.findIn(image)
+        height, width, _ = image.shape
+        goalX, goalY = self.getGoalXYFromLine(line, width, height)
+        # self.publishXY(goalX, goalY)
+        self.lastRightGoal = (goalX, goalY)
+
+    def averageGoal(self):
+        leftX = self.lastLeftGoal[0]
+        leftY = self.lastLeftGoal[1]
+        rightX = self.lastRightGoal[0]
+        rightY = self.lastRightGoal[1]
+        avgX = (leftX + rightX) / 2
+        avgY = (leftY + rightY) / 2
+        return avgX, avgY
+
+    def getCVImageFromData(self, data):
+        return self.bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+
+    @classmethod
+    def getGoalXYFromLine(self, line, width, height):
         try:
-            speed,angle=self.lineDriver.processLine(lineData.data)
-            self.drive(speed,angle)
+            [topX, topY, _, _] = line
+            x = topX - int(width / 2)
+            y = height - topY
+            return x, y
         except Exception:
-            pass
+            return 0, 0
 
-    def scanCallback(self,data):
-        try:
-            speed,angle=self.estop.processInput(data.ranges)
-            self.drive(speed,angle)
-        except Exception:
-            pass
-
-    def drive(self,speed,angle):
-        drive_msg_stamped = AckermannDriveStamped()
-        drive_msg = AckermannDrive()
-        drive_msg.speed = speed
-        drive_msg.steering_angle = angle
-        drive_msg.acceleration = 0
-        drive_msg.jerk = 0
-        drive_msg.steering_angle_velocity = 0
-        drive_msg_stamped.drive = drive_msg
-        self.pub.publish(drive_msg_stamped)
+    def publishXY(self, x, y):
+        self.pub.publish(str(x) + ',' + str(y))
 
 if __name__ == '__main__':
     try:
-        steeringControl()
+        LineFinderSubscriber()
     except rp.ROSInterruptException:
         pass
