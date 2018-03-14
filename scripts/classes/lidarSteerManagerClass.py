9eb322c36c421a73d1f1cb7772101fdec26f0e45
car <car@car.car>
2018-03-14 18:02:12 +0000

    Fixed lidar steering

--- scripts/classes/lidarSteerManagerClass.py
+++ scripts/classes/lidarSteerManagerClass.py
@@ -6,14 +6,19 @@ from constants import LIDAR_STEER_SPEED, LIDAR_PID_P, LIDAR_PID_I, LIDAR_PID_D
 
 class LidarSteerManager:
     def __init__(self):
-        self.pid = PID(PID_P, PID_I, PID_D)
+        self.pid = PID(LIDAR_PID_P, LIDAR_PID_I, LIDAR_PID_D)
         self.pid.SetPoint = 0
         self.pid.setSampleTime(0.01)
 
-    def steer(self, leftDistance, rightDistance):
-        error = rightDistance - leftDistance
+    def steer(self, lidarData):
+        error = self.error(lidarData)
         self.pid.update(error)
         return LIDAR_STEER_SPEED * self.pid.output
+
+    def error(self, lidarData):
+        leftArray, rightArray = lidarData
+        error = sum(rightArray) - sum(leftArray)
+        return error
 
 def main():
     steerManager = LidarSteerManager()
@@ -21,10 +26,10 @@ def main():
         for line in file:
             line = line[:-1] # remove newline at end
             data = line.split(',')
-            leftDistance = float(data[0])
-            rightDistance = float(data[1])
-            steer = steerManager.steer(leftDistance, rightDistance)
-            print('%3.1f -- %3.1f : %3.2f.' % (leftDistance, rightDistance, steer))
+            leftDistances = [float(data[0])]
+            rightDistances = [float(data[1])]
+            steer = steerManager.steer([leftDistances, rightDistances])
+            print('%3.1f -- %3.1f : %3.2f.' % (leftDistances[0], rightDistances[0], steer))
             time.sleep(0.1)
 
 if __name__ == '__main__':
--- scripts/wallCentering.py
+++ scripts/wallCentering.py
@@ -2,6 +2,7 @@
 import rospy as rp
 from sensor_msgs.msg import LaserScan
 from classes.lidarSteerManagerClass import LidarSteerManager
+from std_msgs.msg import String
 
 class WallCentering:
     def __init__(self):
@@ -9,21 +10,18 @@ class WallCentering:
         self.lidarSteer=LidarSteerManager()
 
         self.subscribeToScan()
-        self.pub=rp.Publisher("/wallCenter",AckermannDriveStamped,queue_size=10)
+        self.pub=rp.Publisher("/wallCenter",String,queue_size=10)
         rp.spin()
 
     def subscribeToScan(self):
         rp.Subscriber("scan",LaserScan,self.scanCallback)
 
     def scanCallback(self,data):
-        ranges=[[]]
-        try:
-            ranges[0]=data.ranges[0:360]
-            ranges[1]=data.ranges[720:1080]
-            steerDirection=self.lidarSteer.steer(ranges)
-            self.pub.publish("1,"+steerDirection,",1")
-        except Exception:
-            pass
+        ranges=[[], []]
+        ranges[0]=data.ranges[0:360]
+        ranges[1]=data.ranges[720:1080]
+        steerDirection=self.lidarSteer.steer(ranges)
+        self.pub.publish("1,"+str(steerDirection)+",1")
 
 if __name__ == '__main__':
     try:
