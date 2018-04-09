69a7dd7261a56528c98118b5ea8b8b790434b4a0
car <car@car.car>
2018-04-09 19:37:33 +0000

    Serpentine fixes

--- test_scripts/serpentine.py
+++ test_scripts/serpentine.py
@@ -6,17 +6,19 @@ import rospy as rp
 
 def lidarCallback(data):
     lidarData = data.ranges
-    driveDirection = rightDrive(findRightSmallest(lidarData))
-    pub.publish(driveDirection+",1")
+    smallestRight = findRightSmallest(lidarData)
+    if smallestRight[0] < 1:
+        driveDirection = rightDrive(smallestRight)
+        pub.publish(driveDirection+",1")
 
 def findRightSmallest(arry):
-    rightArray = arry[0:541]
+    rightArray = arry[10:541]
     smallestRight = min(rightArray)
     smallestRightAngle = (arry.index(smallestRight)+1)/4
     return [smallestRight, smallestRightAngle]
 
 def findLeftSmallest(arry):
-    leftArray = arry[542:1080]
+    leftArray = arry[542:1070]
     smallestLeft = min(leftArray)
     print(smallestLeft)
     smallestLeftAngle = (arry.index(smallestLeft)+542)/4
@@ -24,9 +26,9 @@ def findLeftSmallest(arry):
 
 
 def rightDrive(rightSmallest):
-    steer = ((rightSmallest[0] * math.sin(math.radians(90 - rightSmallest[1]))) - 0.61) * .34
-    speed = (rightSmallest[0] * math.cos(math.radians(90 - rightSmallest[1])))
-    return str(.5) + "," + str(steer)
+    steer = ((rightSmallest[0] * math.cos(math.radians(45 - rightSmallest[1]))) - 0.46) * -1#.34
+    speed = (rightSmallest[0] * math.sin(math.radians(45 - rightSmallest[1])))
+    return str(1) + "," + str(steer)
 
 rp.init_node("serpentine",anonymous=False)
 rp.Subscriber("scan",LaserScan,lidarCallback)
